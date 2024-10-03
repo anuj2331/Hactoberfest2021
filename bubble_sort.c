8801b7c23efedc68525596d542949d4eaa4b5e87
anuj2331 <anujm5207@gmail.com>
2024-10-03 18:13:59 +0000

    first commit

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,50 +1,66 @@
-# include <stdio.h>
-# include <stdlib.h>
-void swap(int *ap,int *bp){
-    int temp;
-    temp=*ap;
-    *ap=*bp;
-    *bp=temp;
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *ap, int *bp) {
+    int temp = *ap;
+    *ap = *bp;
+    *bp = temp;
 }
 
-void bubbleSort(int *a,int n){
-    int i,j;
-    for(i=0;i<n-1;i++){
-
-        for(j=0;j<n-i-1;j++){
-
-            if(a[j]>a[j+1])
-                swap(&a[j],&a[j+1]);
+void bubbleSort(int *a, int n) {
+    int i, j;
+    int swapped; // Flag to check if a swap has occurred
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0; // Reset the flag at the start of each outer loop
+        for (j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(&a[j], &a[j + 1]);
+                swapped = 1; // Set the flag to true
+            }
+        }
+        // If no elements were swapped, the array is sorted
+        if (!swapped) {
+            break;
         }
     }
 }
 
-void printArray(int *a,int n){
-    int i;
-    for(i=0;i<n;i++){
-        printf("%d-",a[i]);
+void printArray(int *a, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d", a[i]);
+        if (i < n - 1) {
+            printf(", "); // Better output formatting
+        }
     }
     printf("\n");
 }
 
-int main(){   
+int main() {
     int *a;
-    int n,i,j;
-    
-    printf("enter size of array  : \n");
-    scanf("%d",&n);
-    a=(int*)calloc(n,sizeof(n));
-    
-    for(i=0;i<n;i++){
-        printf("enter element number %d : \n",(i+1));
-        scanf("%d",&a[i]);
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    // Allocate memory for n integers
+    a = (int*)calloc(n, sizeof(int));
+    if (a == NULL) { // Check for successful allocation
+        fprintf(stderr, "Memory allocation failed!\n");
+        return 1;
     }
 
-    printf("The original array is : \n");
-    printArray(a,n);
-    bubbleSort(a,n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter element number %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
 
-    printf("The sorted array is :\n ");
-    printArray(a,n);
+    printf("The original array is:\n");
+    printArray(a, n);
+    bubbleSort(a, n);
+
+    printf("The sorted array is:\n");
+    printArray(a, n);
+
+    free(a); // Free allocated memory
     return 0;
 }
